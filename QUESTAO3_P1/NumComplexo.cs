22871b8df07e64f9cd55b77d57610aa91d43adf4
thiagofcm <thiagof1503@gmail.com>
2022-02-02 17:22:41 -0300

    commit incial

---
+++ QUESTAO3_P1/NumComplexo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QUESTAO3_P1
+{
+    class NumComplexo
+    {
+        //atributos
+        private double Re;
+        private double Im;
+
+        //metodos
+        public NumComplexo() { //construtor default
+            Re = 0.0;
+            Im = 0.0;
+        }
+
+        public NumComplexo(double _Re, double _Im) {
+            Re = _Re;
+            Im = _Im;
+        }
+        //gets
+        public double getRe() {
+            return Re;
+        }
+        public double getIm() {
+            return Im;
+        }
+        //sets
+        public void setRe(double _Re) {
+            Re = _Re;
+        }
+        public void setIm(double _Im) {
+            Im = _Im;
+        }
+        public NumComplexo soma(NumComplexo num2) {
+            double soma_re;
+            double soma_im;
+            soma_re = getRe() + num2.getRe();
+            soma_im = getIm() + num2.getIm();
+            NumComplexo num3 = new NumComplexo(soma_re, soma_im);
+            return num3;
+        }
+        public NumComplexo vezes(NumComplexo num2) {
+            double mult_re;
+            double mult_im;
+            mult_re = (getRe() * num2.getRe())-(getIm()*num2.getIm());
+            mult_im = (getRe() * num2.getIm()) + (getIm()*num2.getRe());
+            NumComplexo num3 = new NumComplexo(mult_re, mult_im);
+            return num3;
+        }
+        public double Modulo() {
+            double modulo;
+            modulo = Math.Sqrt((getRe() * getRe()) + (getIm() * getIm()));
+            return modulo;
+        }
+        public double Argumento() {
+            double tan = (getIm()/getRe());
+            double angulo = Math.Atan(tan);
+            return angulo;
+        }
+        public void imprimeFormaPolar() {
+            double modulo;
+            modulo = Math.Sqrt((getRe() * getRe()) + (getIm() * getIm()));
+            double tan = (getIm() / getRe());
+            double angulo = Math.Atan(tan);
+            Console.WriteLine("A forma polar do numero complexo eh: " + modulo + "(cos " + angulo + " + i sin "+ angulo + ")");
+        }
+
+    }
+}
---
+++ QUESTAO3_P1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QUESTAO3_P1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            NumComplexo z1 = new NumComplexo(1, 1);
+            NumComplexo z2 = new NumComplexo(3, -1);
+            NumComplexo z3 = z1.soma(z2);
+            NumComplexo z4 = z1.vezes(z2);
+            z3.imprimeFormaPolar();
+            z4.imprimeFormaPolar();
+        }
+    }
+}
